edd29d69124a96ee07703c561f9f197ad6c3998b
eomchanu <djacksdn1@icloud.com>
2024-06-14 23:14:14 +0900

    🎉 프로젝트 생성

---
+++ Yakssok/YakssokApp.swift
@@ -0,0 +1,17 @@
+//
+//  YakssokApp.swift
+//  Yakssok
+//
+//  Created by Eom Chanwoo on 6/14/24.
+//
+
+import SwiftUI
+
+@main
+struct YakssokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
